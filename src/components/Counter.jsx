9d186bbc50ac3d6fd2378347f839a1ea0b67051d
omaah2 <ummkhaladoluwatosin@gmail.com>
2023-09-06 13:36:32 +0100

    .

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {
-    ChevronUp,
-    ChevronsUp,
-    RotateCcw,
-    Hash,
-    ChevronDown,
-    ChevronsDown,
+  ChevronUp,
+  ChevronsUp,
+  RotateCcw,
+  Hash,
+  ChevronDown,
+  ChevronsDown,
 } from 'react-feather';
 
 const Counter = () => {
@@ -36,44 +36,43 @@ const Counter = () => {
     setCount(count - 1);
   };
 
-  // Update the document title with the latest count
   useEffect(() => {
     document.title = `Count: ${count}`;
   }, [count]);
-    const mainStyle = {
-        backgroundColor: '#BAA8C5',
-        display: 'flex',
-        justifyContent: 'center',
-        height: '100vh',
-        width: '100vw',
-    }
-  
-    const counterStyle = {
-        display: 'flex',
-        backgroundColor: '#FFFFFF',
-        borderRadius: '5px',
-        boxShadow: '0px 0px 40px rgba(0, 0, 0, 0.5)',
-        padding: '10px',
-        margin: '120px',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '50vh',
-        width: '30vw',
-    }
-     const countStyle = {
-    fontSize: '50px',   
+
+  const mainStyle = {
+    backgroundColor: '#BAA8C5',
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '100vh',
+  };
+
+  const counterStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '10px',
+    margin: '20px',
+    width: '80%',
+    maxWidth: '400px', 
+    backgroundColor: '#FFFFFF',
+    borderRadius: '5px',
+    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.5)',
+  };
+
+  const countStyle = {
+    fontSize: '50px',
     marginTop: '30px',
     display: 'flex',
     paddingBottom: '10px',
     justifyContent: 'center',
-    fontWeight: 'bold',    
-    color: 'blue',         
+    fontWeight: 'bold',
+    color: 'blue',
   };
 
-    const buttonStyle = {
+  const buttonStyle = {
     borderRadius: '5px',
-    backgroundColor: '##808080',
+    backgroundColor: '#808080',
     color: '#000',
     padding: '8px 15px',
     margin: '5px',
@@ -83,32 +82,32 @@ const Counter = () => {
   return (
     <main style={mainStyle}>
       <div style={counterStyle}>
-      <div>
-        <h1>Current Value:</h1>
-      <span style={countStyle}>{count}</span>
+        <div>
+          <h1>Current Value:</h1>
+          <span style={countStyle}>{count}</span>
+        </div>
+        <div>
+          <button onClick={handleIncrementOne} style={buttonStyle}>
+            <ChevronUp />
+          </button>
+          <button onClick={handleIncrementTen} style={buttonStyle}>
+            <ChevronsUp />
+          </button>
+          <button onClick={handleReset} style={buttonStyle}>
+            <RotateCcw />
+          </button>
+          <button onClick={handleRandomize} style={buttonStyle}>
+            <Hash />
+          </button>
+          <button onClick={handleDecrementTen} style={buttonStyle}>
+            <ChevronsDown />
+          </button>
+          <button onClick={handleDecrementOne} style={buttonStyle}>
+            <ChevronDown />
+          </button>
+        </div>
       </div>
-     <div>
-      <button onClick={handleIncrementOne} style={buttonStyle}>
-        <ChevronUp />
-      </button>
-      <button onClick={handleIncrementTen} style={buttonStyle}>
-        <ChevronsUp />
-      </button>
-      <button onClick={handleReset} style={buttonStyle}>
-        <RotateCcw />
-      </button>
-      <button onClick={handleRandomize} style={buttonStyle}>
-        <Hash />
-      </button>
-      <button onClick={handleDecrementTen} style={buttonStyle}>
-        <ChevronsDown />
-      </button>
-      <button onClick={handleDecrementOne} style={buttonStyle}>
-        <ChevronDown/>
-      </button>
-    </div>
-  </div>
-</main>
+    </main>
   );
 };
 
